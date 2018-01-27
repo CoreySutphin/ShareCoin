3b27917baecd0762c1fe6158ff21b3b993f6a14c
cohenta2 <cohenta2@vcu.edu>
2018-01-27 12:41:41 -0500

    Begining twitter connection

--- main.py
+++ main.py
@@ -1,71 +1,18 @@
-from flask import Flask, request, render_template, redirect, url_for, session, g, flash
-from flask_social import Social
-from flask_oauth import OAuth
-import secrets.py import *
+from flask import Flask, request, render_template
+import oauth2
+import secrets
 
 
 app = Flask(__name__)
-DEBUG = True
-app.debug = DEBUG
-app.secret_key = access_token_secret
+def oauth_req(url, key, secret, http_method="GET", post_body='', http_headers=None):
+    consumer = oauth2.Consumer(key=secrets.consumer_key, secret=secrets.consumer_secret)
+    token = oauth2.Token(key=key, secret=secret)
+    client = oauth2.Client(consumer, token)
+    resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers )
+    return content
 
-oauth = OAuth()
-
-twitter = oauth.remote_app('twitter',
-                    base_url='https://api.twitter.com/1/',
-                    request_token_url='https://api.twitter.com/oauth/request_token',
-                    access_token_url='https://api.twitter.com/oauth/access_token',
-                    authorize_url='https://api.twitter.com/oauth/authenticate',
-                    consumer_key = "D1Z6wJrdWHjlEOBb3r3Cmdqer",
-                    consumer_secret = " YgiPCKIRbdhKvQ3JtzzXGj0W4MaprBnhWaNoneWZ0bSmUzgFHk"
-                     )
-
-# app.config['SOCIAL_TWITTER'] = {
-#     'consuer_key' : ,
-#     'consumer_secret' : "YgiPCKIRbdhKvQ3JtzzXGj0W4MaprBnhWaNoneWZ0bSmUzgFHk"
-# }
-
-@twitter.tokengetter
-def get_twitter_token(token=None):
-    return session.get('twitter_token')
-app.config['SECURITY_POST_LOGIN'] = '/profile'
+# home_timeline = oauth_req( 'https://api.twitter.com/1.1/statuses/home_timeline.json', 'abcdefg', 'hijklmnop' )
 
 @app.route('/')
 def index():
-    access_token = session.get('access_token')
-    if access_token is None:
-        return redirect(url_for('login'))
-    access_token = access_token[0]
     return  render_template('index.html', active="index")
-
-@app.route('/login')
-def login():
-    return twitter.authorize(callback=url_for('oauth_authorized', next=request.args.get('next') or request.referrer or None))
-
-@app.route('/logout')
-def logout():
-    session.pop('screen_name', None)
-    flash('You were signed out')
-    return redirect(request.referrer or url_for('index'))
-
-@app.route('/oauth-authorized')
-@twitter.authorized_handler
-def oauth_authorized(resp):
-    next_url = request.args.get('next') or url_for('index')
-    if resp is None:
-        flash(u'you denied the request to sign in.')
-        return redirect(next_url)
-
-    access_token = resp['oauth_token']
-    session['access_token'] = access_token
-    session['screen_name'] = resp['screen_name']
-
-    session['twitter_token'] = (
-        resp['oauth_token'],
-        resp['oauth_token_secret']
-    )
-
-    return redirec(url_for('index'))
-
-if __name__ == '__main__':
-    app.run()
