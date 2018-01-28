adb0acbbecc52c3d686d866a181171dc0f36bffa
Alafran <33580422+Alafran@users.noreply.github.com>
2018-01-27 23:00:46 -0500

    Add files via upload
    Fixed Constructor name and symbol fields

--- ShareCoin.sol
+++ ShareCoin.sol
@@ -1,127 +1,48 @@
 pragma solidity ^0.4.4;
 
-contract Token {
-
-    /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-}
-
-contract StandardToken is Token {
-
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
-}
-
+import "./StandardToken.sol";
 
 //name this contract whatever you'd like
+
 contract ShareCoin is StandardToken {
 
-  // This notifies clients about the amount burnt
+    // This notifies clients about the amount burnt
     event Burn(address indexed from, uint256 value);
-    event TwitterBounty(address userAddress, uint256 value);
+    // Notifies the clients when a Twitter Bounty is successfully created
+    event TwitterBountyCreated(address creatorAddress, uint256 value);
+    //Notifies the clients when a Twitter Bounty is successfully paid out
+    event TwitterBountyPaidOut(address paidOutTo, uint256 amountPaidOut);
+
+    struct Bounty {
+        uint32 shareCoinInBounty;
+        uint32 eachPayout;
+        string uniqueURL;
+    }
+
+    //Array of K,V with URL of tweet as Key, the bounty as value
+    mapping(string => Bounty) public urlToBounty;
+
+
+    /* Public variables of the token */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals.
+    string public symbol;                 //An identifier: eg SBX
+    address private owner;
+
+    function ShareCoin() {
+        owner = msg.sender;
+        balances[owner] = 10000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 10000000;                        // Update total supply (100000 for example)
+        name = "ShareCoin";                                   // Set the name for display purposes
+        decimals = 0;                            // Amount of decimals for display purposes
+        symbol = "SHC";                               // Set the symbol for display purposes
+    }
 
     function () {
         //if ether is sent to this address, send it back.
         throw;
     }
 
-    /* Public variables of the token */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
-    address owner;
-
-    function ShareCoin(
-        ) {
-        owner = msg.sender;
-        balances[msg.sender] = 10000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 10000000;                        // Update total supply (100000 for example)
-        name = "ShareCoin";                                   // Set the name for display purposes
-        decimals = 0;                            // Amount of decimals for display purposes
-        symbol = "SC";                               // Set the symbol for display purposes
-    }
-
-    /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
-    }
 
     /**
      * Destroy tokens
@@ -138,19 +59,45 @@ contract ShareCoin is StandardToken {
         return true;
     }
 
-    // TO-DO: Make this secure!
-    function creditCreators(uint256 _value, address _creator) {
 
-    }
+    // Creates an instance of a Bounty
+    // @param _totalBounty How many total SHCs the bounty is worth
+    // @param _eachPayout How much one payout of the bounty will be in SHC
+    // @param _uniqueURL The URL of the tweet that will be used to identify the bounty
+    // @return will always return true unless Creator does not have enough SHC
+    function createTwitterBounty (
+        uint256 _totalBountyValue,
+        uint256 _eachPayout,
+        string _uniqueURL)
+        public returns (bool success) {
+            //checks if creator has enough SHC to create bounty
+            require(balances[msg.sender] >= _totalBountyValue);
+            //Subtracts bounty amount from creator
+            balances[msg.sender] -= _totalBountyValue;
+            //creates a new bounty with a unique URL
+            Bounty newBounty = Bounty(_totalBountyValue, _eachPayout, _uniqueURL);
+            //adds the new bounty as a value to a map of bounties with creator address as key
+            urlToBounty[_uniqueURL] = newBounty;
+            //Triggers bounty creation event
+            TwitterBountyCreated(msg.sender, _totalBountyValue);
+            return true;
+        }
 
-    // Triggered when a Twitter bouunty to like or retweet a post has been
-    // completed by a user
-    function twitterBountyPayout(uint256 _value, address _creatorAddress) public returns (bool success) {
-        require(balances[_creatorAddress] - _value >= 0);
-
-        balances[msg.sender] += _value;
-
-        TwitterBounty(msg.sender, _value);
+    // Pays a user SHC for completing bounty
+    // Will return false if the bounty does not have the funds to payout
+    // @param address _to is the user wallet address that the funds will be sent to
+    // @param string _uniqueURL is the URL of the tweet used to identify the bounty
+    function payoutTwitterBounty(string _uniqueURL) public returns (bool) {
+        //Reference to the bounty to be manipulated
+        Bounty memory bountyToBePaid = urlToBounty[_uniqueURL];
+        //Returns false if the bounty does not have the balance to pay out
+        require(bountyToBePaid.shareCoinInBounty >= bountyToBePaid.eachPayout);
+        //Subtracts the payout from the bounty
+        bountyToBePaid.shareCoinInBounty -= bountyToBePaid.eachPayout;
+        //Sends the amount of SHC for the bounty to the user
+        transfer(msg.sender, bountyToBePaid.eachPayout);
+        //Triggers bounty payout event
+        TwitterBountyPaidOut(msg.sender, bountyToBePaid.eachPayout);
         return true;
     }
 
