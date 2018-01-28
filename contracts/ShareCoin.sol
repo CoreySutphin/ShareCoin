9443473095d783dea1c7043249ea35121e1868a0
Alafran <33580422+Alafran@users.noreply.github.com>
2018-01-27 23:23:22 -0500

    Add files via upload
    Updated Unique URLs of tweets to keccak256 hashes

--- contracts/ShareCoin.sol
+++ contracts/ShareCoin.sol
@@ -3,30 +3,44 @@ pragma solidity ^0.4.4;
 import "./StandardToken.sol";
 
 //name this contract whatever you'd like
+
 contract ShareCoin is StandardToken {
 
-  // This notifies clients about the amount burnt
+    // This notifies clients about the amount burnt
     event Burn(address indexed from, uint256 value);
-    event TwitterBounty(address userAddress, uint256 value);
+    // Notifies the clients when a Twitter Bounty is successfully created
+    event TwitterBountyCreated(address creatorAddress, uint256 value);
+    //Notifies the clients when a Twitter Bounty is successfully paid out
+    event TwitterBountyPaidOut(address paidOutTo, uint256 amountPaidOut);
+
+    struct Bounty {
+        uint32 shareCoinInBounty;
+        uint32 eachPayout;
+        bytes32 uniqueURL;
+    }
+
+    //Array of K,V with URL of tweet as Key, the bounty as value
+    mapping(bytes32 => Bounty) public urlToBounty;
+
+
+    /* Public variables of the token */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals.
+    string public symbol;                 //An identifier: eg SBX
+    address private owner;
+
+    function ShareCoin() {
+        owner = msg.sender;
+        balances[owner] = 10000000;               // Give the creator all initial tokens (100000 for example)
+        totalSupply = 10000000;                        // Update total supply (100000 for example)
+        name = "ShareCoin";                                   // Set the name for display purposes
+        decimals = 0;                            // Amount of decimals for display purposes
+        symbol = "SHC";                               // Set the symbol for display purposes
+    }
 
     function () {
         //if ether is sent to this address, send it back.
         throw;
-    }
-
-    /* Public variables of the token */
-    //string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    //string public symbol;                 //An identifier: eg SBX
-    address owner;
-
-    function ShareCoin() {
-        owner = msg.sender;
-        balances[msg.sender] = 10000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 10000000;                        // Update total supply (100000 for example)
-        //name = tokenName;                                   // Set the name for display purposes
-        decimals = 0;                            // Amount of decimals for display purposes
-        //symbol = tokenSymbol;                               // Set the symbol for display purposes
     }
 
 
@@ -46,14 +60,58 @@ contract ShareCoin is StandardToken {
     }
 
 
-    // Triggered when a Twitter bouunty to like or retweet a post has been
-    // completed by a user
-    function twitterBountyPayout(uint256 _value, address _creatorAddress) public returns (bool success) {
-        require(balances[_creatorAddress] - _value >= 0);
+    // Creates an instance of a Bounty
+    // @param _totalBounty How many total SHCs the bounty is worth
+    // @param _eachPayout How much one payout of the bounty will be in SHC
+    // @param _uniqueURL The URL of the tweet that will be used to identify the bounty
+    // @return will always return true unless Creator does not have enough SHC
+    function createTwitterBounty (
+        uint32 _totalBountyValue,
+        uint32 _eachPayout,
+        string _uniqueURL)
+        public returns (bool success) {
+            //checks if creator has enough SHC to create bounty
+            require(balances[msg.sender] >= _totalBountyValue);
 
-        balances[msg.sender] += _value;
+            //converts string URL to keccak256 hash
+            bytes32 urlAsHash = keccak256(_uniqueURL);
 
-        TwitterBounty(msg.sender, _value);
+            //Subtracts bounty amount from creator
+            balances[msg.sender] -= _totalBountyValue;
+
+            //creates a new bounty with a unique URL
+            Bounty memory newBounty = Bounty(_totalBountyValue, _eachPayout, urlAsHash);
+
+            //adds the new bounty as a value to a map of bounties with creator address as key
+            urlToBounty[urlAsHash] = newBounty;
+
+            //Triggers bounty creation event
+            TwitterBountyCreated(msg.sender, _totalBountyValue);
+            return true;
+        }
+
+    // Pays a user SHC for completing bounty
+    // Will return false if the bounty does not have the funds to payout
+    // @param address _to is the user wallet address that the funds will be sent to
+    // @param string _uniqueURL is the URL of the tweet used to identify the bounty
+    function payoutTwitterBounty(string _uniqueURL) public returns (bool) {
+        //Converts _uniqueURL to bytes32 hash
+        bytes32 urlAsHash = keccak256(_uniqueURL);
+
+        //Reference to the bounty to be manipulated
+        Bounty memory bountyToBePaid = urlToBounty[urlAsHash];
+
+        //Returns false if the bounty does not have the balance to pay out
+        require(bountyToBePaid.shareCoinInBounty >= bountyToBePaid.eachPayout);
+
+        //Subtracts the payout from the bounty
+        bountyToBePaid.shareCoinInBounty -= bountyToBePaid.eachPayout;
+
+        //Sends the amount of SHC for the bounty to the user
+        transfer(msg.sender, bountyToBePaid.eachPayout);
+
+        //Triggers bounty payout event
+        TwitterBountyPaidOut(msg.sender, bountyToBePaid.eachPayout);
         return true;
     }
 
